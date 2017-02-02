9a7bde0d8dd6bd9f9601396427a424c4e974a665
Jordan <sepheroth0504@yahoo.com>
2017-02-02 15:30:12 -0700

    Added all main menu work

---
+++ Assets/MainMenuScritp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScritp : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ToggleButton(Button button)
+    {
+        if (button.IsActive())
+        {
+            button.gameObject.SetActive(false);
+        }
+        else
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
